cf15b0ee041837d90e95af81e67e5ae113a4968a
RachitAarsh <93877084+RachitAarsh@users.noreply.github.com>
2026-01-28 18:50:49 +0530

    Revert "Features/bookings and payemnts"

--- payment-service/src/main/java/com/cdac/payment/entity/PaymentMode.java
+++
@@ -1,7 +0,0 @@
-package com.cdac.payment.entity;
-
-public enum PaymentMode {
-	   UPI,
-	    CARD,
-	    NET_BANKING
-}
--- payment-service/src/main/java/com/cdac/payment/entity/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.cdac.payment.entity;
-
-public enum PaymentStatus {
-	SUCCESS, FAILED, REFUNDED, PENDING
-}
